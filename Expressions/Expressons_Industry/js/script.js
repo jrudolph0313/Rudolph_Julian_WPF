3380c531a7c2d3e83da1f065e96b9393f8910a62
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-16 16:17:15 -0500

    commenting on what script will do

--- Expressions/Expressons_Industry/js/script.js
+++ Expressions/Expressons_Industry/js/script.js
@@ -1,1 +1,8 @@
-alert("This is in the js file");
+//Julian Rudolph
+//Expressions Industry
+//1-16-14
+//I am a barback.  I change kegs and replace empty liquor bottles all day.
+//I want to calculate how many mug chillers I go through for a given number of kegs
+//Then I want to figure out how much a keg brings in revenue and subtract inital price
+//of the keg.  I want to find the profit of a given number of kegs.
+//1 keg = 55 gal = 7040 oz
