e9bae411fe9ca875d7a443fe6918a4b67cdd5b8b
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-16 16:24:19 -0500

    change scope of script

--- Expressions/Expressons_Industry/js/script.js
+++ Expressions/Expressons_Industry/js/script.js
@@ -1,16 +1,13 @@
 //Julian Rudolph
 //Expressions Industry
 //1-16-14
-//I am a barback.  I change kegs and replace empty liquor bottles all day.
-//I want to calculate how many mug chillers I go through for a given number of kegs
-//Then I want to figure out how much a keg brings in revenue and subtract inital price
-//of the keg.  I want to find the profit of a given number of kegs.
-//1 keg = 55 gal = 7040 oz
+//Find the profit of a given number of kegs
 
 //assigning variables
-//22 oz man mug
-var manMug= 22;
+//ounces of mug input
+var mugOz= prompt("How many ounces is your mug?");
 //input number of kegs
 var numKeg=prompt("How many kegs are available?");
 //check to make sure input works
-console.log(numKeg);
+//console.log(numKeg);
+
