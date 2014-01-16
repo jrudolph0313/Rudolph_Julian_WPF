ac15695c2758dfcfe28517cd75df2db0f0593191
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-16 15:33:49 -0500

    Starting to list vars.

---
+++ Expressions/Expressions_Personal/js/script.js
@@ -0,0 +1,7 @@
+//Expressions Persoal
+//Julian Rudolph
+// 1-16-14
+
+//Calculate the number of gallons of paint to cover a wall (depending on number of coats)
+//prompts and var
+var height= prompt ("Hello, lets calculate the number of gallons of paint you will need to paint a wall. /n Please enter the hight of your wall")
