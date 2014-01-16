7c8219eb782e34480a6b98d8854a13a406b587bf
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-16 15:37:42 -0500

    checking to make sure formula for sqft works

--- Expressions/Expressions_Personal/js/script.js
+++ Expressions/Expressions_Personal/js/script.js
@@ -4,4 +4,8 @@
 
 //Calculate the number of gallons of paint to cover a wall (depending on number of coats)
 //prompts and var
-var height= prompt ("Hello, lets calculate the number of gallons of paint you will need to paint a wall. /n Please enter the hight of your wall")
+var height= prompt ("Hello, lets calculate the number of gallons of paint you will need to paint a wall. /n Please enter the hight of your wall");
+var length= prompt ("What is the length of your wall?");
+//getting sqft from height * width
+var sqft=height*length;
+console.log(sqft)
