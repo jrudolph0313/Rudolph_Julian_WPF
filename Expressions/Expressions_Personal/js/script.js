a1c75d54aa3c3a236c8db383139520096f227b52
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-16 15:46:06 -0500

    added formula to get number of gal needed

--- Expressions/Expressions_Personal/js/script.js
+++ Expressions/Expressions_Personal/js/script.js
@@ -7,13 +7,14 @@
 var height= prompt ("Hello, lets calculate the number of gallons of paint you will need to paint a wall. /n Please enter the hight of your wall");
 var length= prompt ("What is the length of your wall?");
 // Number of coats
-var coats= prompt ("How many coats will you be paint your wall?")
+var coats= prompt ("How many coats will you be paint your wall?");
 //getting sqft from height * width
 var sqft=height*length;
 //assign var gal=350
-var gal=350
+var gal=350;
 //console.log(sqft)
 //sqft times number of coats to get total sqft 
-var totalSqft=sqft * coats
+var totalSqft=sqft * coats;
 //console.log(totalSqft)
 //1 gallon of paint should cover around 350 sqft.  totalSqft / gal = number of gallons
+var galNeed=totalSqft / gal;
