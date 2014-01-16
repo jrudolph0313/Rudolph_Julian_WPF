d8943d8292c68d10a93f98eb3bd6bf8f7d800197
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-16 15:55:10 -0500

    found a new line error, fixed

--- Expressions/Expressions_Personal/js/script.js
+++ Expressions/Expressions_Personal/js/script.js
@@ -4,7 +4,7 @@
 
 //Calculate the number of gallons of paint to cover a wall (depending on number of coats)
 //prompts and var
-var height= prompt ("Hello, lets calculate the number of gallons of paint you will need to paint a wall. /n Please enter the hight of your wall");
+var height= prompt ("Hello, lets calculate the number of gallons of paint you will need to paint a wall. \n Please enter the hight of your wall");
 var length= prompt ("What is the length of your wall?");
 // Number of coats
 var coats= prompt ("How many coats will you be paint your wall?");
