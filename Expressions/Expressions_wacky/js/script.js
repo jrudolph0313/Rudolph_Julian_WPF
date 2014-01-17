5105e87b1b533f1917ecb6814875e3283bb7ad56
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-16 20:25:36 -0500

    define var percentAlcohol

--- Expressions/Expressions_wacky/js/script.js
+++ Expressions/Expressions_wacky/js/script.js
@@ -4,3 +4,5 @@
 //How any ozs of alcohol have you had?
 //asking user for inputs, servig size in oz.
 var sizeBeer=prompt ("How many ounces of beer is in one serving?");
+//percent alcohol in beer
+var percentAlcohol=prompt("What percent of alcohol is your beer?");
