78855870a9780df2c9ee26a3248081323dd2c921
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-22 12:09:27 -0500

    assign var compWeight to a prompt

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -3,3 +3,6 @@
 //1-22-14
 
 // Stuff your face 1
+//competitor must weigh more than 250 lbs to compete
+//assign compWeight to the input of a prompt from user
+var compWeight=prompt("Please input the competitors weight in pounds.");
