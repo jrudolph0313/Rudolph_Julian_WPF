f7801356c67b3df443085e472cf32af7a6c43d6a
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-23 16:19:53 -0500

    Understanding the scope of what to calc and setting up first vars

--- Assignment Conditionals/Conditionals_Industry/js/script.js
+++ Assignment Conditionals/Conditionals_Industry/js/script.js
@@ -1,3 +1,9 @@
 //Julian Rudolph
 //Conditionals Assignment Industry
 //1-23-14
+
+//How much money is being made at a bar on a given day, beer only
+//ask user what the weekly average of beer sold in USD
+var weekAverage=prompt("How much does your bar sell on an average week in $USD?");
+//Check with console
+console.log(weekAverage);
