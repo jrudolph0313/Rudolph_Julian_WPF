ad7efa25db10c871c92f332e7cfcdc9a502b06d9
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-23 18:09:27 -0500

    giving hours an array tbd later

--- Assignment Conditionals/Conditionals_Industry/js/script.js
+++ Assignment Conditionals/Conditionals_Industry/js/script.js
@@ -10,4 +10,8 @@ var weekAverage=prompt("How much does your bar sell on an average week in $USD?"
 //lets find out the day
 var day=prompt("What day is it? Mon, Tues, Wed, Thrus, Fri, Sat, Sun");
 //check with console
-console.log(day);
+//console.log(day);
+//assign number of hours bar is open based on day: sun-wed=13 hours, thrus-sat=15 hours
+var hours=[13, 15];
+//check witn console
+console.log(hours[0], hours[1])
