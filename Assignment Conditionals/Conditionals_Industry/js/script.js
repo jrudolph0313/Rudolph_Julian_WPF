a41ac49de1d4cb83cdaf9001bb08053381c3d7e9
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-23 18:12:24 -0500

    lets see if i can pick the number of hours open based on my if/else statement

--- Assignment Conditionals/Conditionals_Industry/js/script.js
+++ Assignment Conditionals/Conditionals_Industry/js/script.js
@@ -14,4 +14,12 @@ var day=prompt("What day is it? Mon, Tues, Wed, Thrus, Fri, Sat, Sun");
 //assign number of hours bar is open based on day: sun-wed=13 hours, thrus-sat=15 hours
 var hours=[13, 15];
 //check witn console
-console.log(hours[0], hours[1])
+//console.log(hours[0], hours[1])
+//decide how many hours depending on which day is selected
+if (day="Mon", "Tues", "Wed", "Sun"){
+	hours=hours[0];
+}else{
+	hours=hours[1];
+}
+//check with console
+console.log(hours);
