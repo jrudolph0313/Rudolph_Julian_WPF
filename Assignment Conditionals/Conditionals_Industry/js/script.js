456434bd8fdbd99e66c7d9532e36315a1d9aaf58
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-23 16:24:19 -0500

    made array and checking with console.log

--- Assignment Conditionals/Conditionals_Industry/js/script.js
+++ Assignment Conditionals/Conditionals_Industry/js/script.js
@@ -6,4 +6,8 @@
 //ask user what the weekly average of beer sold in USD
 var weekAverage=prompt("How much does your bar sell on an average week in $USD?");
 //Check with console
-console.log(weekAverage);
+//console.log(weekAverage);
+//The numbers are made up, depending on which day will determine how much can be attributed to the day the user chooses.  since there are only 3 different percentages in a week, i will make these an array
+var percentDaily=new array(.05, .1, .35);
+//check with console
+console.log(percentDaily[1]);
