6da594a97c854113bcc47908be524eb6a2d2e817
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-23 10:22:32 -0500

    starting personal calc: litecoin profit or loss calc.

--- Assignment Conditionals/Conditionals_Personal/js/script.js
+++ Assignment Conditionals/Conditionals_Personal/js/script.js
@@ -1,3 +1,11 @@
 //Julian Rudolph
 //Assignment Conditionals_Personal
 //1-23-14
+//How much could a person have made investing in Litecoin
+//input old price of Litecoin, input how much is USD would have been invested, divide to get number of 
+//Litecoin that would have been recieved, multiply that by the current price.  If user made money one 
+//result will appear.  If user lost money a different result will appear.
+//assign vars to prompts
+var amountInvested=prompt("How much would you have invested in LTC had you not believed the negitive media propaganda?");
+//make sure it works
+console.log(amountInvested);
