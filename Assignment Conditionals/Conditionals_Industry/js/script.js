f9baf60b7be80b73cca5efe991c8cbaa567c020f
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-23 18:22:16 -0500

    tring to learn if a the correct number of hoursOpen is returned in console.

--- Assignment Conditionals/Conditionals_Industry/js/script.js
+++ Assignment Conditionals/Conditionals_Industry/js/script.js
@@ -11,15 +11,12 @@ var weekAverage=prompt("How much does your bar sell on an average week in $USD?"
 var day=prompt("What day is it? Mon, Tues, Wed, Thrus, Fri, Sat, Sun");
 //check with console
 //console.log(day);
-//assign number of hours bar is open based on day: sun-wed=13 hours, thrus-sat=15 hours
-var hours=[13, 15];
-//check witn console
-//console.log(hours[0], hours[1])
-//decide how many hours depending on which day is selected
-if (day="Mon", "Tues", "Wed", "Sun"){
-	hours=hours[0];
-}else{
-	hours=hours[1];
-}
+//if/else for hours assigned to a var
+var hoursOpen;
+	if (day="Mon", "Tues", "Wed", "Sun"){
+		hoursOpen=13;
+	} else{
+		hoursOpen=15;
+	}
 //check with console
-console.log(hours);
+console.log(hoursOpen);
