23cdc28b4198c087313f689842c501de458ffed9
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-23 20:21:59 -0500

    first alert to an empty input

--- Assignment Conditionals/Conditionals_Industry/js/script.js
+++ Assignment Conditionals/Conditionals_Industry/js/script.js
@@ -5,6 +5,10 @@
 //How much money is being made at a bar on a given day, beer only
 //ask user what the weekly average of beer sold in USD
 var weekAverage=prompt("How much does your bar sell on an average week in $USD?");
+	if(weekAverage==""){
+		alert("The amount your bar sells on an average week must be entered!")
+		prompt("How much does your bar sell on an average week in $USD?")
+	}
 //Check with console
 //console.log(weekAverage);
 //lets find out the day
