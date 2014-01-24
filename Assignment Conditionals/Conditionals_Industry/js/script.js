f73db173c3a2738f09d21354dff74b9317896617
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-23 19:16:21 -0500

    lets turn days into numbers and see what happens

--- Assignment Conditionals/Conditionals_Industry/js/script.js
+++ Assignment Conditionals/Conditionals_Industry/js/script.js
@@ -8,32 +8,30 @@ var weekAverage=prompt("How much does your bar sell on an average week in $USD?"
 //Check with console
 //console.log(weekAverage);
 //lets find out the day
-var day=prompt("What day is it? Mon, Tues, Wed, Thrus, Fri, Sat, Sun");
+var day=prompt("What day is it? 1-Mon, 2-Tues, 3-Wed, 4-Thrus, 5-Fri, 6-Sat, 7-Sun. Please use numbers, not name of day!");
 //check with console
-//console.log(day);
+console.log(day);
+
+/*
 //assign hours open to an array
 var hoursOpen=[13, 15];
 //check with console
 //console.log(hoursOpen[1]);
-//assign percentages of sales accounted for days in array
-var percentDay=[.05, .1, .35]
-//check with console
-//console.log(percentDay[2]);
 //lets do math
-var totalForDay;
+var totalForDay="";
  
- if (day=="Mon", "Tues", "Wed", "Thurs"){
-	//console.log(day);
-	totalForDay=weekAverage * percentDay[0];
-} else if(day=="Sat", "Sun"){
-	totalForDay=weekAverage * percentDay[2]
-} else {
-	totalForDay=weekAverage * percentDay[1]
-}
+	if(day=1, 2, 3, 4){
+		//console.log(day);
+		totalForDay=(weekAverage * 0.05)
+	}else if(day=6, 7){
+		totalForDay=(weekAverage * 0.35)
+	}else{
+		totalForDay=(weekAverage * 0.1)
+	}
 
 //check with console
 console.log(totalForDay);
-
+*/
 
 
 
