83af72d518955a4828695d68f0bd02aeb2516338
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-23 21:46:20 -0500

    ok, lets see where this takes me. basic concept

--- Assignment Conditionals/Conditionals_Wacky/js/script.js
+++ Assignment Conditionals/Conditionals_Wacky/js/script.js
@@ -1,1 +1,13 @@
-alert("This is in the js file");
+//Julian Rudolph
+//conditionals wacky
+//1-23-14
+
+//Is it a good idea to ride a friend down a hill?
+//assuming the fried agrees with the idea
+//depending on users friends size in relation to the users, angle of slope, and what kind of cover
+//is over the hill (snow, ice, dry)
+
+//alert user to what we are going to decide
+alert("Today we are going to determine if it is a good idea to ride a friend down a hill.");
+//ask user if their friend
+var sizeFriend=prompt("Would you consider your friend to be larger than yourself?");
