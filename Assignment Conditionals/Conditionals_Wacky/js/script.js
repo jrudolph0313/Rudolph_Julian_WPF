c5f2923a448ab38c73cee2153725aeec735f91be
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-23 21:53:16 -0500

    adding slope

--- Assignment Conditionals/Conditionals_Wacky/js/script.js
+++ Assignment Conditionals/Conditionals_Wacky/js/script.js
@@ -10,4 +10,9 @@
 //alert user to what we are going to decide
 alert("Today we are going to determine if it is a good idea to ride a friend down a hill.");
 //ask user if their friend
-var sizeFriend=prompt("Would you consider your friend to be larger than yourself?");
+var sizeFriend=prompt("Would you consider your friend to be larger than yourself? yes or no?");
+//check with console
+//console.log(sizeFriend);
+var slope=prompt("what degree is the slope of the hill?")
+//check
+console.log(slope);
