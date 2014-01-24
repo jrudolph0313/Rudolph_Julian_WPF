656f725de073425a2f8470ab9f4c07337168bcd6
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-23 20:02:07 -0500

    Figured out if else if else statement...turns out it was expecting a condition (hence: if(conditon1)...

--- Assignment Conditionals/Conditionals_Industry/js/script.js
+++ Assignment Conditionals/Conditionals_Industry/js/script.js
@@ -10,28 +10,28 @@ var weekAverage=prompt("How much does your bar sell on an average week in $USD?"
 //lets find out the day
 var day=prompt("What day is it? 1-Mon, 2-Tues, 3-Wed, 4-Thrus, 5-Fri, 6-Sat, 7-Sun. Please use numbers, not name of day!");
 //check with console
-console.log(day);
+//console.log(day);
 
-/*
+
 //assign hours open to an array
 var hoursOpen=[13, 15];
 //check with console
 //console.log(hoursOpen[1]);
-//lets do math
-var totalForDay="";
- 
-	if(day=1, 2, 3, 4){
-		//console.log(day);
-		totalForDay=(weekAverage * 0.05)
-	}else if(day=6, 7){
-		totalForDay=(weekAverage * 0.35)
-	}else{
-		totalForDay=(weekAverage * 0.1)
-	}
 
-//check with console
-console.log(totalForDay);
-*/
+
+//lets do math
+
+var priceOnDay;
+if(day<5){
+	priceOnDay=weekAverage*0.05;
+}else if(day==5){
+	priceOnDay=weekAverage*0.1;
+}else{
+	priceOnDay=weekAverage*0.35;
+}
+//checked math with console
+console.log(priceOnDay);
+
 
 
 
