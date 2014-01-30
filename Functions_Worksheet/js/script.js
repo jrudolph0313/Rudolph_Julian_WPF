f1c041345d173fc1aa4746dabe89f1e2e58a0bf1
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-30 11:00:49 -0500

    testing math with console

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -2,3 +2,15 @@
 //Funcitons Worksheet
 // 1-30-14
 
+//circumference
+
+var radius=prompt("What is the raidus of your circle?")
+var diameter= radius*2
+//Check with console
+console.log(diameter);
+
+/*
+function circumference(){
+
+}
+*/
