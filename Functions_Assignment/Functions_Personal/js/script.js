dae77eb7215a0e4419f70cc12d2a47711db73bee
jrudolph0313 <j.rudolph0313@gmail.com>
2014-01-31 16:03:20 -0500

    checking math

--- Functions_Assignment/Functions_Personal/js/script.js
+++ Functions_Assignment/Functions_Personal/js/script.js
@@ -6,6 +6,13 @@
 
 alert("Lets see a good example of how Fractional Reserve Banking works.");
 
-var USD=prompt("Input a number in $USD.")
+var USD=prompt("Input a deposit amount in $USD.")
 //check with console
-console.log(USD);
+//console.log(USD);
+
+bankKeep(USD, .10)
+
+function bankKeep(USD, Law){
+	total=USD*Law
+	console.log(total)
+}
