8b8deb76327c7313feeb50791d17253d155a6c41
jrudolph0313 <j.rudolph0313@gmail.com>
2014-02-01 18:28:01 -0500

    Functions Personal Final

--- Functions_Assignment/Functions_Personal/js/script.js
+++ Functions_Assignment/Functions_Personal/js/script.js
@@ -1,12 +1,12 @@
 //Julian Rudolph
 //Functions Personal
-//1-30-14
+//2-1-14
 
 //How does fractional reserve banking work
 
 alert("Lets see a good example of how Fractional Reserve Banking works.");
 
-var USD=prompt("Input a deposit amount in $USD.")
+var USD=prompt("Input a deposit amount in $USD. (HINT) It is suggested that the user doesn't put a large amount or the 'while' loop will print too much to the console")
 //check with console
 //console.log(USD);
 
@@ -33,14 +33,13 @@ function bankLoan(USD, total){
 
 console.log("If you deposited $"+USD+" "+"in your bank they have to keep only 10% of that.  This means that your bank is allowed to loan $"+bankLoan+" "+"to anyone they want. What happens when that customer deposits that amount into their own account?  Lets find out!")
 
-//var peopleDeposit=bankLoan*.9;
-//console.log(peopleDeposit);
-
+//starting while loop
 while (USD>=1){
 	USD=USD*.9
 	console.log(USD)
 }
 
+alert("The numbers in the console are the amounts, in $USD, that can be loaned out just from your initial deposit amount. Assuming that each person deposits the money that was loaned to them into their own account. Now you understand the Mystery of Banking!")
 
 
 
