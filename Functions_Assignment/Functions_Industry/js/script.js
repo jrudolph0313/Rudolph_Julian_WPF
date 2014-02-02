5f584b77fa99f874d20e41c657efd0eb7e6fd3f7
jrudolph0313 <j.rudolph0313@gmail.com>
2014-02-01 19:45:36 -0500

    starting and checking industry function EBAC calc

--- Functions_Assignment/Functions_Industry/js/script.js
+++ Functions_Assignment/Functions_Industry/js/script.js
@@ -1,3 +1,15 @@
 //Julian Rudolph
 //Functions assignment industry
 //2-1-14
+
+//estimating blood alcohol content
+//the code is all mine, the formula i found online.  I am not going to get into the specifics, just basics.
+
+var bodyWater=prompt("Hello, lets estimate your blood alcohol content!  Are you a male or female?")
+	if(male){
+		bodyWater=.58
+	}else{
+		bodyWater=.49
+	}
+//check with console
+console.log(bodyWater);
